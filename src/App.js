4d8af079f4f4fcf5e24e8826162501bd176d70e7
ruthhawino <awinoruthh@gmail.com>
2023-06-25 11:55:26 +0300

    created headline

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Introduction from './Introduction';
+
+function App() {
+  return (
+    <div>
+      <Introduction/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Introduction/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css"
+
+const Introduction =()=>{
+    return(
+      <div>
+        <h1>What I learnt in React</h1>
+        <p>It is  JS library Intalledby Facebook</p>
+        <p>It has reusablecomonents</p>
+        
+      </div>
+      
+    )
+    
+  }
+  
+  export default Introduction
+  
